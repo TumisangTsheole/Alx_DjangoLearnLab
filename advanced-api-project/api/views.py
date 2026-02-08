c1dae64915e9e14d32831630927fbbf68115a94f
Tumisang <tsheoletumisang934@gmail.com>
2026-02-08 16:29:38 +0000

    fix views

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics, status
-from rest_framework.permissions import IsAuthenticatedOrReadOnly,  IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework.response import Response
 
 from .models import Book
