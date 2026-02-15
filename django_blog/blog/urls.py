c61b9956409fac83ede1ded3ffe70d6ef866d0d0
Tumisang <tsheoletumisang934@gmail.com>
2026-02-15 18:15:54 +0000

    Refactor project structure and settings to align with updated requirements.txt and fix static file loading

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -13,8 +13,7 @@ from .views import (
 )
 
 urlpatterns = [
-    path('', PostListView.as_view(), name='blog-home'),
-    path('search/', PostListView.as_view(), name='search'), # Added search URL
+    path('', PostListView.as_view(), name='home'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -31,46 +31,79 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+
     'django.contrib.admin',
+
     'django.contrib.auth',
+
     'django.contrib.contenttypes',
+
     'django.contrib.sessions',
+
     'django.contrib.messages',
+
     'django.contrib.staticfiles',
+
     'blog',
-    'crispy_forms',
+
     'taggit',
+
 ]
 
-CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
-CRISPY_TEMPLATE_PACK = "bootstrap4"
+
 
 MIDDLEWARE = [
+
     'django.middleware.security.SecurityMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
+
     'django.middleware.common.CommonMiddleware',
+
     'django.middleware.csrf.CsrfViewMiddleware',
+
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+
     'django.contrib.messages.middleware.MessageMiddleware',
+
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
+
+
 
 ROOT_URLCONF = 'django_blog.urls'
 
 
+
+
+
 TEMPLATES = [
+
     {
+
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR/ "templates"],
-        'APP_DIRS': True,
+
+        'DIRS': [BASE_DIR / 'templates'],
+
         'OPTIONS': {
+
             'context_processors': [
+
+                'django.template.context_processors.debug',
+
                 'django.template.context_processors.request',
+
                 'django.contrib.auth.context_processors.auth',
+
                 'django.contrib.messages.context_processors.messages',
+
             ],
+
         },
+
     },
+
 ]
 
 WSGI_APPLICATION = 'django_blog.wsgi.application'
