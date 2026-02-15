c233f70ee2d6054bebd5592af367fe49ccefaf2d
Tumisang <tsheoletumisang934@gmail.com>
2026-02-15 14:33:23 +0000

    Added auth views, profile management

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -11,3 +11,12 @@ class Post(models.Model):
         User,
         on_delete=models.CASCADE
     )
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s profile"
+
